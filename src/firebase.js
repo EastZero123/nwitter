d4d4260cbf748874dc802c3520d39448e2cc462b
EastZero123 <cdy1005p@naver.com>
2022-07-22 20:43:37 +0900

    2장 완료

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYB_R3te25lkT1qWrfCJiFL7qNfssszxo",
+  authDomain: "nwitter-3657d.firebaseapp.com",
+  projectId: "nwitter-3657d",
+  storageBucket: "nwitter-3657d.appspot.com",
+  messagingSenderId: "694992490946",
+  appId: "1:694992490946:web:0164ee914065b362c4065b",
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
